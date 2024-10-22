5ef1ff37780bf7e33aa4391db37526ef2fc172ba
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-10-22 01:15:12 -0300

    feat: initial setup

---
+++ PetitionsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetitionsProject
+//
+//  Created by Rodrigo on 22-10-24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
