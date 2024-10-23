4b27944c2fd08c5433f146c2e0b495f525e84ffa
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-10-23 00:37:59 -0300

    feat: Add petitions fetching and cell config

--- PetitionsProject/ViewController.swift
+++ PetitionsProject/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UITableViewController {
     override func viewDidLoad() { 
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let response = try? decoder.decode(Petitions.self, from: json) {
+            petitions = response.results
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,8 +38,11 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        let petition = petitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
